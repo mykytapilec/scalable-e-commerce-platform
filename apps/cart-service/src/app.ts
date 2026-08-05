f02d630b7eb5425354afa5e9313e05901d4deadd
Mikita Pilets <mykytapilec@gmail.com>
2026-08-05 19:00:48 +0300

    feat: initialize cart service

---
+++ apps/cart-service/src/app.ts
@@ -0,0 +1,20 @@
+import Fastify from 'fastify';
+import logger from '@ecommerce/logger';
+import { config } from '@ecommerce/config';
+import healthRoute from './routes/health.js';
+
+const app = Fastify({
+  logger,
+});
+
+app.register(healthRoute);
+
+app.log.info(
+  {
+    environment: config.nodeEnv,
+    port: config.port,
+  },
+  'Cart Service initialized',
+);
+
+export default app;
