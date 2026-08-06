1568c376d7d8cd133d113fd85313a05105e62aae
Mikita Pilets <mykytapilec@gmail.com>
2026-08-06 22:29:09 +0300

    refactor: migrate user service to prisma client

--- apps/user-service/src/database.ts
+++ apps/user-service/src/database.ts
@@ -1,5 +1,5 @@
-import { checkDatabaseConnection } from "@ecommerce/database";
+import { prisma } from "@ecommerce/database";
 
 export async function initializeDatabase(): Promise<void> {
-  await checkDatabaseConnection();
+  await prisma.$connect();
 }
